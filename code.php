907cb38751bbc9a91aec2ea1e53721085d2d9bd6
Hector Juan Soria <hector151293@gmail.com>
2015-10-02 09:04:50 +0000

    Nuevo archivo para probar el codigo ahi

--- code.php
+++ code.php
@@ -1,3 +1,4 @@
+
 <?php
 //Array multilenguaje
 $lang="en";
@@ -41,7 +42,7 @@ $menu=array(
 ?>
 <html>
     <head>
-        <title>Docuemento de prueba</title>
+        <title>Documento de prueba</title>
         <meta charset="UTF-8">
  <!-- Bootstrap -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
@@ -50,31 +51,9 @@ $menu=array(
     </head>
     <body>
         
-        <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="#">Math dice</a>
-            </div>
-            <div>
-              <ul class="nav navbar-nav">
-                <?php //foreach ($menu as $item): ?>
-                <li class="active"><a href="#">Home</a></li>
-                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                      
-                    <li><a href="#">Page 1-1</a></li>
-                    <li><a href="#">Page 1-2</a></li>
-                    <li><a href="#">Page 1-3</a></li>
-                    
-                  </ul>
-                </li>
-                <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
-                <?php //endforeach; ?>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        
+        <?php foreach ($menu as $item): ?>
+                <li class="active"><a href="#"><?php echo $item["titulo"][$lang] ?></a></li>
+            <?php endforeach; ?>
+
     </body>
 </html>
